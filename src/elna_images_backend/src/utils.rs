8c6764aa9e0a735194155a227fb32da42e7246c3
Mohit Rajan E <mhit98@gmail.com>
2024-12-06 23:09:31 +0530

    bug:adding new asset after deleting one can result in duplicate id's

--- src/elna_images_backend/src/utils.rs
+++ src/elna_images_backend/src/utils.rs
@@ -1,9 +1,14 @@
-use super::{Error, OWNER};
+use super::{Error, STATE};
 use candid::Principal;
-use std::cell::RefCell;
 
 pub fn check_if_owner(caller: Principal) -> Result<(), Error> {
-    let owner = OWNER.with(|owner: &RefCell<String>| owner.borrow().clone());
+    let owner = STATE.with(|state| {
+        state
+            .borrow()
+            .get(&"owner".to_string())
+            .unwrap_or("".to_string())
+    });
+
     if caller.to_string() != owner {
         ic_cdk::println!("owner:{}\tcaller:{}", owner, caller);
         return Err(Error::Unauthorized);
